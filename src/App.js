1bccbf636f8ca59794897b3b2621852bface62d7
Gustavo Henrique <gugahribeiro@hotmail.com>
2020-07-20 18:38:19 -0300

    chore/config/app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyled from './styles/global';
+
+import Header from './components/Header'
+import Routes from './routes';
+
+
+// import { Container } from './styles';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Olá</h1>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<GlobalStyled />
+			<Header />
+			<Routes />
+		</BrowserRouter>
+	)
 }
-
 export default App;
